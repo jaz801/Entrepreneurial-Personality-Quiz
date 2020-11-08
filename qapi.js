e40b4b87acfb2de2afbec1eae83b279e4d7b2afe
jaz801 <34124216+jaz801@users.noreply.github.com>
2020-11-08 09:33:59 +0100

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,37 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa7acdccae167001e0619e1",
+				"5fa7acdccae167001e0619e3",
+				"5fa7ad15b1c8af001eb4bc88",
+				"5fa7acedb83a2f001f2d72b2"
+			]
+		},
+		"5fa7acdccae167001e0619e1": {
+			"id": "5fa7acdccae167001e0619e1",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa7acdccae167001e0619e3": {
+			"id": "5fa7acdccae167001e0619e3",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"5fa7acedb83a2f001f2d72b2": {
+			"id": "5fa7acedb83a2f001f2d72b2",
+			"pageUrl": "q1",
+			"name": "Q1"
+		},
+		"5fa7ad15b1c8af001eb4bc88": {
+			"id": "5fa7ad15b1c8af001eb4bc88",
+			"pageUrl": "about-us",
+			"name": "About-US"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
